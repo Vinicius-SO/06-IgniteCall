d62325315550f4eb88b86c2e2b151a00168fbf24
unknown <vhisoares39@gmail.com>
2024-01-17 20:55:43 -0300

    Add: Form functionality

--- src/pages/Home/components/ClainUserForm/index.tsx
+++ src/pages/Home/components/ClainUserForm/index.tsx
@@ -1,15 +1,30 @@
 import { TextInput, Button } from "@ignite-ui/react"
 import {ArrowRight} from 'phosphor-react'
+
 import { Form } from "./styles"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+const claimUsernameFormSchema = z.object({
+    username: z.string()
+})
+
 
 export function ClaimUsernameForm(){
+
+    const { register, handleSubmit } = useForm()
+
+    async function handleClaimUserName(data: any){
+        console.log(data)
+    }
     return(
-       <Form>
+       <Form as="form" onSubmit={handleSubmit(handleClaimUserName)}>
             <TextInput 
                 size='sm'
                 prefix="ignite.com/"
                 placeholder="seu-usuario"
-                crossOrigin=''
+                {...register('username')} 
+                crossOrigin={undefined}
             />
             <Button size='sm' type='submit'>
                 Reservar 
