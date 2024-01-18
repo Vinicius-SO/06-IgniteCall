b7ef8189473c9639951b6df9579c6d10fc590248
unknown <vhisoares39@gmail.com>
2024-01-18 20:26:45 -0300

    Add: Adding cookies for username

--- src/pages/api/users/index.api.ts
+++ src/pages/api/users/index.api.ts
@@ -1,4 +1,5 @@
 import { prisma } from "@/lib/prisma";
+import { setCookie } from 'nookies'
 import type { NextApiRequest, NextApiResponse } from "next";
 import { z } from "zod";
 
@@ -21,14 +22,30 @@ export default async function handlers(
         return res.status(405).end() // METHOD NOT ALLOWED
     }
 
+
     const {name, username}  = UserSchema.parse(req.body)
 
+    const userExists = await prisma.user.findUnique({
+        where: {
+            username
+        }
+    })
     
+    if (userExists){
+        return res.status(400).json({
+            message:'Username already exist'
+        })
+    }
     const user = await prisma.user.create({
         data: {
             username,
             name,
         },
+    })
+
+    setCookie({res}, '@ignitecall:userId', user.id, {
+        maxAge: 60 * 60 * 24 * 7, // 7days
+        path: '/',
     })
 
 
